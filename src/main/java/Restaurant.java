cb4c107a48121e8e3a84947edecc5ae817368af4
fgoc <feyzullahgoctr@gmail.com>
2026-05-13 11:21:43 +0200

    feyzu

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,27 +1,21 @@
+import util.TextUI;
+
 import java.util.ArrayList;
 
 public class Restaurant {
-	private ArrayList<User> users;
-	private ArrayList<Table> tables;
-	private MenuCard foodMenu;
-	private MenuCard dessertMenu;
-	private MenuCard drinksMenu;
-	private Menu userMenu;
-	private DBConnector dbConnector;
+	public static ArrayList<User> users;
+	public static ArrayList<Table> tables;
+	public static MenuCard foodMenu;
+	public static MenuCard dessertMenu;
+	public static MenuCard drinksMenu;
+	public static Menu userMenu;
+	public static DBConnector dbConnector;
 
 	public Restaurant() {
 		dbConnector = new DBConnector();
 	}
 
-	public Restaurant(ArrayList<User> users, ArrayList<Table> tables, MenuCard foodMenu, MenuCard dessertMenu, MenuCard drinksMenu, Menu userMenu) {
-		this.users = users;
-		this.tables = tables;
-		this.foodMenu = foodMenu;
-		this.dessertMenu = dessertMenu;
-		this.drinksMenu = drinksMenu;
-		this.userMenu = userMenu;
-		dbConnector = new DBConnector();
-	}
+
 
 	public void initialize() {
 		// Henter data fra databasen
@@ -29,12 +23,31 @@ public class Restaurant {
 		users = dbConnector.selectUser();
 		tables = dbConnector.selectTable();
 		foodMenu = dbConnector.selectFoodMenu();
-		dessertMenu = null;
-		drinksMenu = null;
+		dessertMenu = dbConnector.selectdessertMenu();
+		drinksMenu = dbConnector.selectdrinksMenu();
 		userMenu = new WaiterMenu(tables);
 	}
 
 	public Menu getUserMenu() {
 		return userMenu;
 	}
+
+	public Menu ChooseMenu() {
+
+		int input = TextUI.promptNumeric("1.Waiter\n2.Manager\n3.Kitchen Staff");
+
+		if (input == 1) {
+			return new WaiterMenu(tables);
+		} else if (input == 2) {
+			return new ManagerMenu();
+		} else if (input == 3) {
+			return new KitchenMenu();
+		} else {
+			return ChooseMenu();
+		}
+
+	}
+
+
+
 }
